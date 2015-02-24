eb6f084be077653547ab131517d2b864b16799b4
DClaudio <david_claudio_t@yahoo.com>
2015-02-24 11:26:45 +0000

    first commit

---
+++ basics.rb
@@ -0,0 +1,30 @@
+require 'rubygems'
+require 'sinatra'
+
+get '/' do
+	"Hello, World!"
+end
+
+get '/about' do
+	"A little about me."
+end
+
+get '/hello/:name' do
+	"Hello there, #{params[:name].upcase}."
+end
+
+
+get '/hello/:name/:city' do
+  "Hey there #{params[:name]} from #{params[:city]}."
+end
+
+get '/more/*' do
+	params[:splay]
+end
+
+get '/form' do
+	erb :form
+end
+post '/form' do
+	"You said '#{params[:message]}'"
+end
